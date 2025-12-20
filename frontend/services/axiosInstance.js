cc6e3d0bd7470208a5501e75750df0bbf4837664
AthmakuruKavya <129976204+AthmakuruKavya@users.noreply.github.com>
2025-12-20 12:30:51 +0530

    update axiosInstance.js

--- frontend/services/axiosInstance.js
+++ frontend/services/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://memory-vault-backend-c2bi.onrender.com',
+  baseURL: 'https://memory-vault-backend-c2bi.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
